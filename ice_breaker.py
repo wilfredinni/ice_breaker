c8354ffcd0e1e32eff0c259a7044ace324c67878
Carlos <carlos@pythoncheatsheet.org>
2023-05-07 12:16:32 -0400

    minor fix

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,18 +7,6 @@ from third_parties.linkedin import scrape_linkedin_profile
 
 load_dotenv(find_dotenv())
 
-information = """
-Elon Reeve Musk FRS (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business
-magnate and investor. He is the founder, CEO and chief engineer of SpaceX;
-angel investor, CEO and product architect of Tesla, Inc.; owner and CEO of
-Twitter; founder of the Boring Company; co-founder of Neuralink and OpenAI;
-and president of the philanthropic Musk Foundation. With an estimated net
-worth of around $192 billion as of March 27, 2023, primarily from his
-ownership stakes in Tesla and SpaceX,[4][5] Musk is the second-wealthiest
-person in the world, according to both the Bloomberg Billionaires Index and
-Forbes's real-time billionaires list.
-"""
-
 if __name__ == "__main__":
     summary_template = """
     Given the LinkedIn information {info} about a person from I
