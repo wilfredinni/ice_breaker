6d8f2b29482ad02ffd384ad58e19ba4dc0f34131
Carlos <carlos@pythoncheatsheet.org>
2023-05-07 15:16:43 -0400

    feat: parse output with Pydantic

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,11 +7,12 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.twitter import scrape_user_tweets
+from output_parsers import person_intel_parser, PersonIntel
 
 load_dotenv(find_dotenv())
 
 
-def ice_break(name: str) -> str:
+def ice_break(name: str) -> PersonIntel:
     linkedin_profile_url = linkedin_lookup_agent(name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url)
     twitter_username = twitter_lookup_agent(name)
@@ -24,20 +25,24 @@ def ice_break(name: str) -> str:
     2. Two interesting facts about the person
     3. A topic that may interest the person
     4. A creative Ice Breaker to open a conversation with the person
+    \n{summary_format}
     """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_info", "twitter_info"],
         template=summary_template,
+        partial_variables={
+            "summary_format": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     result = chain.run(linkedin_info=linkedin_data, twitter_info=tweets)
-    return result
+    return person_intel_parser.parse(result)
 
 
 if __name__ == "__main__":
-    name = "Harrison Chase"
+    name = "Guido van Rossum"
     result = ice_break(name)
     print(result)
